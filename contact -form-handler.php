720a90c2cc4f886411fd22962f832a65913bea0e
james stimpson <jws19900715@gmail.com>
2020-07-06 22:36:23 -0400

    added php code to get contact form working

---
+++ contact -form-handler.php
@@ -0,0 +1,20 @@
+<?php
+ $name = $_POST['name'];
+ $visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'james-stimpson@lambdastudents.com
+
+$email_subject = "new form submission";
+
+$email_body = "User Name: $name.\n" .
+              "User Email: $visitor_email.\n".
+               "User Message: $message.\n";
+
+$to = "jws19900715@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html");
