d329106d6546dc295f4ed4d66e32d9b628fafc28
james stimpson <jws19900715@gmail.com>
2020-07-06 22:40:29 -0400

    fixed syntax error

--- contact -form-handler.php
+++ contact -form-handler.php
@@ -3,7 +3,7 @@
  $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-$email_from = 'james-stimpson@lambdastudents.com
+$email_from = 'james-stimpson@lambdastudents.com'
 
 $email_subject = "new form submission";
 
